502b9d5c111a72f4adc4d82ec371c98cc2d09876
farxat60@gmail.com <farxat60@gmail.com>
2018-10-05 13:16:56 +0300

    Initial Commit

---
+++ WheatherApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WheatherApplication
+//
+//  Created by Joe Franc on 10/5/18.
+//  Copyright © 2018 farxat60. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
